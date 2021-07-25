d991eff12cc6bf1cc489f2b608c10fc269d8e713
Taline Joventino <taline_peres29@hotmail.com>
2021-07-25 19:03:08 -0300

    Filtragem de pokemons da div promoção

---
+++ frontend/src/services/DatasCarousel/PokesPromotion.ts
@@ -0,0 +1,21 @@
+const PokesPromotion = [
+    {
+        id: 25,
+        name: 'Pikachu',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+        types: 'eletric',
+        types2: '',
+        abilities: 'static',
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+// https://pokeapi.co/ pesquisa o pokemon pra achar os seu id e características
+// caso a id do pokemon seja so um numero vc coloca dois zeros antes da url da imagem ex: 005.png, for dois numeros coloca 1, e se for tres nenhum
+
+export default PokesPromotion;
