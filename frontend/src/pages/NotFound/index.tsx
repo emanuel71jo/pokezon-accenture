17d1a1aa0bd3ae7f037c01feb34fed09534592a3
Barbara Costa <barbaradscosta14@gmail.com>
2021-08-03 17:21:57 -0300

    Estilizar tela Not Found

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -1,5 +1,18 @@
-import { Container } from './styles'
+import { Container, Frase } from './styles'
+import img from '../../assets/zzz.gif'
 
 export function NotFound() {
-  return <Container>NotFound</Container>
+  return <Container>
+      
+      <div>
+        <img src= { img } alt="Imagem Pokémon" />
+      </div>
+      
+      <Frase>
+      <span>Oooops...</span>      
+        <h1>ERROR 404</h1>
+        <span>Page not found :( </span>
+      </Frase>
+    
+    </Container>
 }
--- frontend/src/pages/NotFound/styles.ts
+++ frontend/src/pages/NotFound/styles.ts
@@ -1,3 +1,62 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    margin: auto;
+        img {
+        margin-top: 7rem;
+        margin-bottom: 8rem;
+        height: 15rem;
+        }   
+
+        
+  @media screen and (max-width: 425px) {
+  display: flex;
+  flex-direction: row;
+  margin-top:5rem;
+  margin-bottom: 5.4rem;
+  img {
+        margin-top: 7rem;
+        height: 7rem;
+        }  
+}
+   
+
+`
+
+export const Frase = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 2rem;
+    h1 {
+        font-size: 3rem;
+        margin-top: 0.5rem;
+    }
+    span {
+        font-size: 2rem;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+
+  @media screen and (max-width: 425px) {
+  display: flex;
+  flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h1 {
+        font-size: 1rem;
+        margin-top: 0.5rem;
+    }
+    span {
+        font-size: 1rem;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+
+  }
+`
